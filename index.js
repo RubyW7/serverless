352d6bd80444cc57d19b37ea7739e033688d517f
YidanWWW <wang.yidan2@northeastern.edu>
2024-12-05 19:31:30 -0500

    add

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 import AWS from 'aws-sdk';
 import formData from 'form-data';
 import Mailgun from 'mailgun.js';
+import {
+ SecretsManagerClient,
+ GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+
+const secret_name = "email_service_credentials2";
+
+
+const client = new SecretsManagerClient({
+ region: "us-east-1",
+});
+
+
+let response;
+
+
+try {
+ response = await client.send(
+   new GetSecretValueCommand({
+     SecretId: secret_name,
+     VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+   })
+ );
+} catch (error) {
+ // For a list of exceptions thrown, see
+ // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+ throw error;
+}
+
+
+const secret = JSON.parse(response.SecretString);
+console.log(secret.apikey)
+
 
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
 username: 'api',
-key: process.env.MAILGUN_API_KEY
+key: secret.apikey
 });
+
 
 export const handler = async (event) => {
 const message = JSON.parse(event.Records[0].Sns.Message);
@@ -18,12 +53,16 @@ const token = message.userToken;  // get token from sns
 
 
 try {
-  // use Mailgun send email
-  const emailResponse = await mg.messages.create(process.env.MAILGUN_DOMAIN, {
-    from: 'no-reply@demo.rubyw.xyz',
-    to: [email],
-    subject: "Welcome to our website! Please confirm your email",
-    text: `Hello ${firstName},
+ // use Mailgun send email
+ const emailResponse = await mg.messages.create("demo.rubyw.xyz", {
+   from: 'no-reply@demo.rubyw.xyz',
+   to: [email],
+   subject: "Welcome to our website! Please confirm your email",
+   text: `Hello ${firstName},
+
+
+
+
 
 
 
@@ -33,7 +72,15 @@ Welcome to our website! We're excited to have you here.
 
 
 
-Please click on the following link to verify your email address: http://demo.rubyw.xyz/v1/user/verifyUserEmail?email=${email}&token=${token}
+
+
+
+
+Please click on the following link to verify your email address: https://demo.rubyw.xyz/v1/user/verifyUserEmail?email=${email}&token=${token}
+
+
+
+
 
 
 
@@ -43,9 +90,17 @@ If you did not request this, please ignore this email.
 
 
 
+
+
+
+
 Best Regards,
 Your Company Name`,
-    html: `<p>Hello ${firstName},</p>
+   html: `<p>Hello ${firstName},</p>
+
+
+
+
 
 
 
@@ -55,7 +110,15 @@ Your Company Name`,
 
 
 
-<p>Please click on the following link to verify your email address: <a href="http://demo.rubyw.xyz/v1/user/verifyUserEmail?email=${email}&token=${token}">Verify Email</a></p>
+
+
+
+
+<p>Please click on the following link to verify your email address: <a href="https://demo.rubyw.xyz/v1/user/verifyUserEmail?email=${email}&token=${token}">Verify Email</a></p>
+
+
+
+
 
 
 
@@ -63,10 +126,12 @@ Your Company Name`,
 <p>If you did not request this, please ignore this email.</p>
 
 
+
+
 `
-  });
-  console.log('Email sent:', emailResponse);
+ });
+ console.log('Email sent:', emailResponse);
 } catch (error) {
-  console.error('Error sending email:', error);
+ console.error('Error sending email:', error);
 }
 };
